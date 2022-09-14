762209df28a241704ecc069e630f7316d879e519
Dalton Martins <daltonvlm@gmail.com>
2022-09-14 10:32:32 -0300

    Fix new line printing. Wrap variables with "

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 MY_BASHRC=`ls my_bashrc 2> /dev/null`
 
-for f in `ls -I $0`;
+for f in `ls -I $0 -I README.md`;
 do
-	ln -fs $PWD/$f ~/.$f
+	ln -fs "$PWD/$f" ~/."$f"
 done
 
-test $MY_BASHRC && echo "\n. ~/.$MY_BASHRC" >> ~/.bashrc
+test "$MY_BASHRC" && echo -e "\n. ~/.$MY_BASHRC" >> ~/.bashrc
