9f72e064678656b7147126617b13d11ef1e4ab03
Dalton Martins <daltonvlm@gmail.com>
2022-10-13 21:17:50 -0300

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-MY_BASHRC=`ls my_bashrc 2> /dev/null`
-
 for f in `ls -I $0 -I README.md`;
 do
 	ln -fs "$PWD/$f" ~/."$f"
 done
 
-test "$MY_BASHRC" && echo -e "\n. ~/.$MY_BASHRC" >> ~/.bashrc
+test -f "bashrc.sh" && echo -e "\nsource ~/.bashrc.sh" >> ~/.bashrc
+test -f "vimrc.vim" && echo -e "\nsource ~/.vimrc.vim" >> ~/.vimrc
